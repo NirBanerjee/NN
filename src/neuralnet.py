4888793ed42f1580d462dbd0dea8344cc9928bc3
Nirmoho Banerjee <nban12345@gmail.com>
2018-03-10 14:18:41 -0500

    Initial Commit

---
+++ src/neuralnet.py
@@ -0,0 +1,9 @@
+from __future__ import division
+import numpy as np 
+
+np.set_printoptions(precision=8, suppress=True)
+
+#Main Method
+if __name__ == '__main__':
+
+	#Get all command line arguments
